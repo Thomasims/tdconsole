7d314439839a606a5e8ed9f6003ece3324abdc9c
Thomas <3007463+Thomasims@users.noreply.github.com>
2024-04-27 13:50:41 +0200

    Update dllmain.cpp for 1.5.4

--- dllmain.cpp
+++ dllmain.cpp
@@ -181,12 +181,14 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <stdio.h>
 
 BOOL APIENTRY DllMain(HMODULE, DWORD ul_reason_for_call, LPVOID)
 {
 	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
 	{
 		AllocConsole();
+		freopen("CONOUT$", "w", stdout);
 	}
 	return TRUE;
 }
